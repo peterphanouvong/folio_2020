81cb3836ef8ac67ae568891badf20fec05096889
Peter Phanouvong <peterphanouvong@gmail.com>
2020-12-24 15:43:24 +1100

    usehistory import

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react"
-import { useHistory } from "react-router-dom"
+import React, { useState, useEffect, useHistory } from "react"
 import MyLink from "./myLink"
 
 const Navbar = ({ showAboutWindows }) => {
